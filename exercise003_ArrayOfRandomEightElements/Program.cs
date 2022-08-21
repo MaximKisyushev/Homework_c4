6f716c7e3065f08e00bb81d152e5ce7f78cb1a8d
MaximKisyushev <kisyushevmax@yandex.ru>
2022-08-21 12:31:29 +0500

    add exercise 003 Arrayofrandomeghtelements(1, 100)

---
+++ exercise003_ArrayOfRandomEightElements/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void FillArray(int[] collection)
+{
+    for (int i = 0, enterLength = collection.Length; i < enterLength; i++)
+    {
+        collection[i] = new Random().Next(1, 100);
+    }
+}
+
+void PrintArray(int[] col)
+{
+    for (int count = 0, printLength = col.Length; count < printLength; count++)
+    {
+        Console.Write($"{col[count]}\t");
+    }
+}
+
+int[] array = new int[8];
+FillArray(array);
+Console.Write("[");
+PrintArray(array);
+Console.Write("]");
